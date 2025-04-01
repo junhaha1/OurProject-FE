51604f39267af690b83c8b38ef7ca3ddd214546c
junhaha1 <wnsgk919@naver.com>
2025-04-01 17:22:17 +0900

    first setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import BoardList from './testview/board/BoardList';
-import AddArticle from './testview/board/AddArticle';
-import ArticleDetail from './testview/board/ArticleDetail'; // ✨ 상세 페이지 컴포넌트
+import BoardListView from './testview/board/BoardListView'; //메인화면 컴포넌트
+import DetailView from './testview/board/DetailView'; //게시글 상세보기 컴포넌트
+import LoginView from './testview/login/LoginView';  //로그인 화면 컴포넌트
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<BoardList />} />
-      <Route path="/add" element={<AddArticle />} />
-      <Route path="/view/:id" element={<ArticleDetail />} /> {/* ✨ 동적 라우팅 */}
+      <Route path="/" element={<BoardListView />} /> {/* 메인 화면 */}
+      <Route path="/login" element={<LoginView />}/> {/* 로그인 화면*/}
+      <Route path="/viewarticle/:id" element={<DetailView />} /> {/* 게시글 상세보기 */}
     </Routes>
   );
 }
